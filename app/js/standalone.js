e3539ef3a087ca4ba433438d835de5df7ce439b3
Lalitha Pranathi Pulavarthy <drprans@icloud.com>
2026-01-14 13:29:43 -0500

    restored ehr selection dropdown

--- app/js/standalone.js
+++ app/js/standalone.js
@@ -1,11 +1,15 @@
 let selectedEHR = null;
 
-document.getElementById('ehrSelect').addEventListener('change', function() {
-    selectedEHR = this.value;
-    const customForm = document.getElementById('customForm');
-    if (selectedEHR === 'custom') customForm.classList.add('show');
-    else customForm.classList.remove('show');
-    document.getElementById('launchBtn').disabled = !selectedEHR;
+document.querySelectorAll('.ehr-option').forEach(option => {
+    option.addEventListener('click', function() {
+        document.querySelectorAll('.ehr-option').forEach(opt => opt.classList.remove('selected'));
+        this.classList.add('selected');
+        selectedEHR = this.dataset.ehr;
+        const customForm = document.getElementById('customForm');
+        if (selectedEHR === 'custom') customForm.classList.add('show');
+        else customForm.classList.remove('show');
+        document.getElementById('launchBtn').disabled = false;
+    });
 });
 
 document.getElementById('launchBtn').addEventListener('click', function() {
